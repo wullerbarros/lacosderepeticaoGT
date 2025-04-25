cc78a6b7329d5a601c5bce94f7d045ca84e02279
wullerbarros <wuller1987@gmail.com>
2025-04-25 16:51:17 -0300

    exercicio lacos de repetiçao

---
+++ lacos.js
@@ -0,0 +1,37 @@
+// for(inicializacao; condicao; incremento){
+
+// }
+
+let js = "javascript"
+
+for(let i = 0; i < js.length; i++){
+    alert(js[i])
+
+}
+
+//array //for
+
+let frutas = [
+    "maça",
+   "banana",
+   "uva",
+   "melancia"
+]
+
+for(let i = 0; i < frutas.length; i++){
+    alert(frutas[i])
+
+}
+for(let i = 3; i >= 0; i--){
+    alert(frutas[i])
+
+}
+
+//while
+
+let i = 1;
+while (i <= 5){
+    console.log("contando"+ 1)
+}
+
+let valor = parseInt(prompt("adivinhe o numero"))
